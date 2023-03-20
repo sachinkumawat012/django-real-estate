fd0b103166a14d69b4e06a17e0120b6dccc68674
sachinkumawat012 <sachinkumawat012@gmail.com>
2023-03-20 11:21:30 +0530

    msg: splite setting configrations

---
+++ real_estate/real_estate/settings/development.py
@@ -0,0 +1,13 @@
+from .base import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': env('PG_ENGINE'),
+        'NAME': env('PG_NAME'),
+        'USER': env('PG_USER'),
+        'PASSWORD': env('PG_PASSWORD'),
+        'HOST': env('PG_HOST'),
+        'PORT': env('PG_PORT')
+    }
+}
