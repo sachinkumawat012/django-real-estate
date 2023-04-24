825631f22cf83d7678dc85daedad6a33c3f989f2
sachinkumawat012 <sachinkumawat012@gmail.com>
2023-04-24 13:26:22 +0530

    feat: add rating model, serializer in the rating app

--- real_estate/apps/ratings/models.py
+++ real_estate/apps/ratings/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from real_estate.settings.base import AUTH_USER_MODEL
+from apps.common.models import TimeStampedUUIDModle
+from apps.profiles.models import Profile
 
 # Create your models here.
+
+class Rating(TimeStampedUUIDModle):
+
+    class Range(models.IntegerChoices):
+        RATING_1 = 1, _("Poor")
+        RATING_2 = 2, _("Fair")
+        RATING_3 = 3, _("Good")
+        RATING_4 = 4, _("Very Good")
+        RATING_5 = 5, _("Excelent")
+
+    rater = models.ForeignKey(
+                AUTH_USER_MODEL,
+                verbose_name=_("User providing the rating"),
+                on_delete=models.SET_NULL,
+                null=True,
+                )
+    agent  = models.ForeignKey(
+                Profile,
+                verbose_name = _("Agent being rated"), 
+                on_delete=models.SET_NULL,
+                null=True,
+                related_name="agent_review"
+                )
+    rating = models.IntegerField(
+                verbose_name=_("Rating"),
+                choices=Range.choices,
+                help_text="1=Poor, 2=Fail, 3=Good, 4=Very Good, 5=Excelent",
+                default=0,
+                )
+    commet = models.TextField(verbose_name=_("Comments"))
+
+    class Meta:
+        unique_together = ["rater", "agent"]
+
+    def __str__(self) -> str:
+        return f"{self.agent} rated at {self.rating}"
